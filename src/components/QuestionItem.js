509ec0d2fe6e822914f608722f70e10ab26cead4
Luis Escobar <lae311@stern.nyu.edu>
2021-11-08 16:20:01 -0500

    commit tree 2c67a8bc771067c9051aae9c893e1172d10e0678

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onNewAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDeleteClick () {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method : "DELETE"
+    })
+    .then(r=>r.json())
+    .then(() => onDeleteQuestion(question))
+  }
+
+  function handleCorrectAnswerChange (e) {
+    const newAnswerIndex = e.target.value
+    console.log(newAnswerIndex)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      Headers:
+      { "Content-Type": "application/json" },
+      Body: JSON.stringify(
+        {"correctIndex": newAnswerIndex}
+      )
+    })
+    .then(r=>r.json())
+    .then((updatedQuestion) => onNewAnswer(updatedQuestion))
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +38,12 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select 
+        defaultValue={correctIndex}
+        onChange={handleCorrectAnswerChange}
+        >{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
